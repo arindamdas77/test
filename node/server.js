57c4b26a30766a8f4f0f7aac90fafaa5fabdf7a0
arindam <dasarindam@gmail.com>
2025-12-18 06:07:53 +0000

    m

--- node/server.js
+++ node/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from Express Frontend on AWS!</h1>");
+  res.send("<h1>Hello from Express Frontend Pipline not AWS!</h1>");
 });
 
 app.listen(3000, () => console.log);
