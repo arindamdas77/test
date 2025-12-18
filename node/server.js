c7fed60dde9473934b6dc2a1a4b5dc67efc6fe61
arindam <dasarindam@gmail.com>
2025-12-18 05:49:07 +0000

    test

---
+++ node/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Express Frontend on AWS!</h1>");
+});
+
+app.listen(3000, () => console.log);
+
