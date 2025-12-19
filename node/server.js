77f9c060459914d67b4941cb29d3363ebc6b6b6f
arindam <dasarindam@gmail.com>
2025-12-19 10:33:38 +0000

    m

--- node/server.js
+++ node/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1> Express ARINDAM DAS not AWS!</h1>");
+  res.send("<h1> Express ARINDAM DAS its a Pipline on Jankins!</h1>");
 });
 
 app.listen(3000, () => console.log);
